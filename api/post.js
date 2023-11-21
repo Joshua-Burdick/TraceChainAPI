b5f6eaa70d3e22aaf4acb72c18a4359a5c3c7a58
lamywmpaul@gmail.com <lamywmpaul@gmail.com>
2023-11-20 21:25:46 -0500

    Feed endpoint

--- api/post.js
+++ api/post.js
@@ -10,6 +10,18 @@ router.get('/', (req, res) => {
   }
 })
 
+router.get(`/feed`, async (req, res) => {
+  console.log("GENERATING FEED...");
+  try {
+    const data = await Post.find({}).sort({ createdAt: -1});
+    console.log(data);
+    res.json(data);
+  } catch (error) {
+    console.error("An error occurred when trying to generate Feed: ", error);
+    res.status(500).json({ message: 'Error occurred while Feeding' });
+  }
+})
+
 router.get('/:id', async (req, res) => {
   const { id } = req.params;
   console.log("Receiveed request with ID", id);
@@ -17,18 +29,14 @@ router.get('/:id', async (req, res) => {
     const idAsObjectId = new mongoose.Types.ObjectId(id);
     const data = await Post.findById(idAsObjectId).exec();
     console.log(data);
-    res.json({ data });
+    res.json(data);
   } catch (error) {
     console.error("An error occurred when trying to find the id: ", error);
     res.status(500).json({ message: 'Error occurred while fetching data' });
   }
 })
 
-router.get('/:id', async (req, res) => {
-  const { id } = req.params;
-  const data = await Post.findById(id);
-  res.json({ data });
-});
+
 
 
 // router.put('/put_the_data/:id', async (req, res) => {
