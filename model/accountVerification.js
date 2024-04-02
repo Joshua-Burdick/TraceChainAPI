bc787bb7cf53d432ea24a6c17c4d61e110e77489
amraibra <92942609+amraibra@users.noreply.github.com>
2024-04-02 14:15:16 -0400

    Bio updates

--- model/accountVerification.js
+++ model/accountVerification.js
@@ -3,7 +3,8 @@ const { Schema, model } = require('mongoose');
 const AccountVerificationSchema = new Schema({
     username: String,
     displayName: String,
-    email: String
+    email: String,
+    bio: String
 })
 
 const AccountVerification = model('AccountVerification', AccountVerificationSchema, 'accountTest');
